04941c25d42ffadd4dc5e1e2b064bab92e6b81ef
Miuuuosz <102805763+Miuuuosz@users.noreply.github.com>
2022-05-04 18:03:53 +0200

    Add files via upload

---
+++ search.js
@@ -0,0 +1,4 @@
+    /*jslint devel: true */
+    var user_input = prompt("Podaj elementy ktore chcesz wyszukac ponizej. Wstaw . jezeli szukasz klas, wstaw # jezeli szukasz id...", "np. .div, #id...");
+    const elements = document.querySelectorAll(user_input);
+    console.log(elements);
